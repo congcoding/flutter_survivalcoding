0971fbacdb44bfc78f6aa27a7d279e3062119c27
congcoding <congcoding@gmail.com>
2020-11-19 22:35:39 +0900

    [FEAT] Chapter 4.2.7 (GridView)

--- lib/main.dart
+++ lib/main.dart
@@ -22,26 +22,29 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('제목'),
       ),
-      body: ListView(
-        scrollDirection: Axis.vertical,
+      body: GridView.count(
+        crossAxisCount: 2,
         children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Home'),
-            trailing: Icon(Icons.navigate_next),
-            onTap: () {},
+          Container(
+            color: Colors.red,
+            width: 100,
+            height:100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
           ),
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Event'),
-            trailing: Icon(Icons.navigate_next),
-            onTap: () {},
+          Container(
+            color: Colors.green,
+            width: 100,
+            height:100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
           ),
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text('Camera'),
-            trailing: Icon(Icons.navigate_next),
-            onTap: () {},
+          Container(
+            color: Colors.blue,
+            width: 100,
+            height:100,
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(8.0),
           ),
         ]
       )
