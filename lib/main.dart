14e47e34034945b1ea2982a8fced0f0887ba266a
congcoding <congcoding@gmail.com>
2020-11-19 23:20:12 +0900

    [FEAT] Week 03 응용 (전화 자판 만들기)

--- lib/main.dart
+++ lib/main.dart
@@ -16,23 +16,29 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<String> items = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '*', '0', '#'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text('Home'),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person),
-          title: Text('Profile'),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.notifications),
-          title: Text('Notification'),
-        ),
-      ])
+      appBar: AppBar(
+        title: Text('전화 자판 만들기'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,  // 열 개수
+        children: List<Widget>.generate(items.length, (idx) {
+          return Container(
+            color: Colors.amber,
+            padding: const EdgeInsets.all(40),
+            margin: const EdgeInsets.all(8),
+            child: Text(
+                items[idx],
+                style: TextStyle(fontSize: 40),
+                textAlign: TextAlign.center,
+            ),
+          );
+        }).toList()
+      )
     );
   }
 }
