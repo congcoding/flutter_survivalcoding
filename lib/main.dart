642d21dabda11939f9fd53a88014b532e7754026
congcoding <congcoding@gmail.com>
2021-01-07 18:25:22 +0900

    [FEAT] Chapter 5.1.1 (TextField)

--- lib/main.dart
+++ lib/main.dart
@@ -15,58 +15,29 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var _text = '';
-  final List<String> items = ['C', '+', '-', '*', '/', '=', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var gridList = List<Widget>.generate(items.length, (idx) {
-      return Container(
-          width: 50,
-          child: RaisedButton(
-              color: Colors.amber,
-              child: Text(
-                items[idx],
-                style: TextStyle(fontSize: 40),
-                textAlign: TextAlign.center,
-              ),
-              onPressed: () {
-                setState(() {
-                  if (items[idx] == 'C')
-                    _text = '';
-                  else
-                    _text += items[idx];
-                });
-              }));
-    }).toList();
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('계산기'),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Container(
-                  height: 50,
-                  child: Text(_text, style: TextStyle(fontSize: 40))),
-              Container(
-                  height: 400,
-                  child: GridView.count(
-                    crossAxisCount: 4,
-                    children: gridList,
-                  ))
-            ],
-          ),
+        appBar: AppBar(
+          title: Text('TextField'),
         ),
-      ),
+        body: Column(
+          children: <Widget>[
+            TextField(),
+            TextField(
+                decoration: InputDecoration(
+                  labelText: '여기에 입력하세요',
+                ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: '여기에 입력하세요',
+              ),
+            )
+          ]
+        )
     );
   }
 }
