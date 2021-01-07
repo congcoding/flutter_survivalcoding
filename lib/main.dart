6115eb2669cd61449bea006156bc02fdb894b644
congcoding <congcoding@gmail.com>
2021-01-07 18:54:19 +0900

    [FEAT] Chapter 5.1.3 (Radio, RadioListTile)

--- lib/main.dart
+++ lib/main.dart
@@ -20,40 +20,71 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+enum Gender { MAN, WOMEN}
+
 class _MyHomePageState extends State<MyHomePage> {
-  var _isChecked = false;
+  Gender _gender = Gender.MAN;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Checkbox, Switch'),
+          title: Text('Radio, RadioListTile'),
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Checkbox(
-                  value: _isChecked,
-                  onChanged: (value) {
-                    setState(() {
-                      _isChecked = value;
-                    });
-                  },
+                ListTile(
+                  title: Text('남자'),
+                  leading: Radio(
+                    value: Gender.MAN,
+                    groupValue: _gender,
+                    onChanged: (value) {
+                      setState(() {
+                        _gender = value;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: Text('여자'),
+                  leading: Radio(
+                    value: Gender.WOMEN,
+                    groupValue: _gender,
+                    onChanged: (value) {
+                      setState(() {
+                        _gender = value;
+                      });
+                    },
+                  ),
                 ),
                 SizedBox(
                   height: 40,
                 ),
-                Switch(
-                  value: _isChecked,
+                RadioListTile(
+                  title: Text('남자'),
+                  value: Gender.MAN,
+                  groupValue: _gender,
                   onChanged: (value) {
                     setState(() {
-                      _isChecked = value;
+                      _gender = value;
                     });
-                  }
-                )
+                  },
+                ),
+                RadioListTile(
+                  title: Text('여자'),
+                  value: Gender.WOMEN,
+                  groupValue: _gender,
+                  onChanged: (value) {
+                    setState(() {
+                      _gender = value;
+                    });
+                  },
+                ),
               ]
             )
           )
