6a26bcd3f75396f8c028a50a1a22fac08ed0aa23
congcoding <congcoding@gmail.com>
2021-01-07 21:43:54 +0900

    [FEAT] Chapter 5.5.1 (CupertinoDesign)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -15,29 +16,41 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final _items = List.generate(50, (i) => ListTile(title: Text('No. $i')));
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _isOn = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: true,
-            expandedHeight: 180.0,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text('SliverList'),
-              background: Image.asset(
-                'assets/sample.jpg',
-                fit: BoxFit.cover,
-              )
-            )
+      appBar: CupertinoNavigationBar(
+        middle: Text('CupertinoDesign'),
+      ),
+      body: Column(
+        children: <Widget>[
+          CupertinoSwitch(
+            value: _isOn,
+            onChanged: (bool value) {
+              setState(() {
+                _isOn = value;
+              });
+            },
           ),
-          SliverList(
-            delegate: SliverChildListDelegate(_items),
+          CupertinoButton(
+            borderRadius: BorderRadius.circular(16.0),
+            color: Colors.orange,
+            child: Text('쿠퍼티노 AlertDialog'),
+            onPressed: () {},
           ),
-        ],
+          CupertinoButton(
+            child: Text('쿠퍼티노 Picker'),
+            onPressed: () {},
+          )
+        ]
       )
     );
   }
