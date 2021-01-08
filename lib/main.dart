e9392672d1ead003adc450c5194e83362cc438c1
congcoding <congcoding@gmail.com>
2021-01-08 11:55:03 +0900

    [FEAT] Week 06 응용 (학점 계산기)

--- lib/main.dart
+++ lib/main.dart
@@ -10,72 +10,189 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
-      home: FirstStatefulPage(),
-      routes: {
-        '/first': (context) => FirstStatefulPage(),
-        '/second': (context) => SecondStatefulPage(),
-      }
+      home: GradeMain(),
     );
   }
 }
 
-class Person {
-  String name;
-  int age;
-
-  Person(this.name, this.age);
-}
-
-class FirstStatefulPage extends StatefulWidget {
+class GradeMain extends StatefulWidget {
   @override
-  _FirstStatefulPageState createState() => _FirstStatefulPageState();
+  _GradeMainState createState() => _GradeMainState();
 }
 
-class _FirstStatefulPageState extends State<FirstStatefulPage> {
+class _GradeMainState extends State<GradeMain> {
+  List<Widget> _children = [];
+  List _gradeList = ['A+'];
+  List _creditList = ['3'];
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _add();
+  }
+
   @override
   Widget build(BuildContext context) {
-    print('FirstPage build()');
     return Scaffold(
       appBar: AppBar(
-        title: Text('First'),
+        title: Text('학점 계산기')),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              IconButton(icon: Icon(Icons.add), onPressed: _add),
+              Flexible(
+                child: ListView(children: _children),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                alignment: Alignment.centerRight,
+                child: RaisedButton(
+                  onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => GradeResult(_gradeList, _creditList)),
+                      );
+                    },
+                  child: Text('결과'),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-      body: RaisedButton(
-        child: Text('다음 페이지로'),
-        onPressed: () {
-          final person = Person('홍길동', 20);
-              Navigator.push(
-              context,
-            MaterialPageRoute(builder: (context) => SecondStatefulPage(person: person)),
-          );
-        }
+    );
+  }
+
+  final _gradeValueList = ['A+', 'A-', 'A0', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D+', 'D0', 'D-', 'F'];
+  var _gradeSelectedValue = 'A+';
+  final _creditValueList = ['3', '2', '1'];
+  var _creditSelectedValue = '3';
+
+  void _add() {
+    _children = List.from(_children)
+      ..add(Row(
+        children: <Widget>[
+          Container(
+            width: 100,
+            child: TextFormField(
+              decoration: InputDecoration(hintText: "과목명"),
+            ),
+          ),
+          SizedBox(width: 10),
+          Container(
+            width: 100,
+            child: DropdownButtonFormField(
+              value: _gradeSelectedValue,
+              items: _gradeValueList.map(
+                  (value) {
+                    return DropdownMenuItem(
+                      value: value,
+                      child: Text(value),
+                    );
+                  },
+              ).toList(),
+              onChanged: (value) {
+                setState(() {
+                  _gradeSelectedValue = value;
+                  _gradeList.add(_gradeSelectedValue);
+                });
+              }
+            )
+          ),
+          SizedBox(width: 10),
+          Container(
+              width: 100,
+              child: DropdownButtonFormField(
+                  value: _creditSelectedValue,
+                  items: _creditValueList.map(
+                        (value) {
+                      return DropdownMenuItem(
+                        value: value,
+                        child: Text(value),
+                      );
+                    },
+                  ).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _creditSelectedValue = value;
+                      _creditList.add(_creditSelectedValue);
+                    });
+                  }
+              )
+          ),
+        ],
       )
     );
+    setState(() {});
   }
 }
 
-class SecondStatefulPage extends StatefulWidget {
-  final Person person;
-  SecondStatefulPage({Key key, @required this.person}) : super(key: key);
+class GradeResult extends StatelessWidget {
+  final List gradeList;
+  final List creditList;
 
-  @override
-  _SecondStatefulPageState createState() => _SecondStatefulPageState();
-}
+  GradeResult(this.gradeList, this.creditList);
 
-class _SecondStatefulPageState extends State<SecondStatefulPage> {
   @override
   Widget build(BuildContext context) {
-    print('SecondPage build()');
+    print(gradeList);
+    print(creditList);
+    double totalGrade = 0.0;
+    int totalCredit = 0;
+    for (int i = 0; i < gradeList.length; i++) {
+      totalGrade += _calcGrade(gradeList[i], creditList[i]);
+      totalCredit += int.parse(creditList[i]);
+    }
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.person.name),
+      appBar: AppBar(title: Text('학점 계산기')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'GPA\n' + (totalGrade / totalCredit).toStringAsFixed(2),
+              style: TextStyle(fontSize: 36),
+            ),
+          ],
+        ),
       ),
-      body:
-        RaisedButton(
-          child: Text('이전 페이지로'),
-          onPressed: () {
-            Navigator.pop(context);
-          }
-        )
     );
+  }
+
+  double _calcGrade(String grade, String credit) {
+    double result = 0;
+    if (grade == 'A+') {
+      result += 4.5;
+    } else if (grade == 'A0') {
+      result += 4.2;
+    } else if (grade == 'A-') {
+      result += 4.0;
+    } else if (grade == 'B+') {
+      result += 3.6;
+    } else if (grade == 'B0') {
+      result += 3.3;
+    } else if (grade == 'B-') {
+      result += 3.0;
+    } else if (grade == 'C+') {
+      result += 2.6;
+    } else if (grade == 'C0') {
+      result += 2.3;
+    } else if (grade == 'C-') {
+      result += 2.0;
+    } else if (grade == 'D+') {
+      result += 1.6;
+    } else if (grade == 'D0') {
+      result += 1.3;
+    } else if (grade == 'D-') {
+      result += 1.0;
+    } else {
+      result += 0.0;
+    }
+    return result * (int.parse(credit));
   }
 }
