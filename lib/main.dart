bbbb62d22427adb73ffb73f900f8e06109d899dd
congcoding <congcoding@gmail.com>
2021-01-13 21:34:30 +0900

    [FEAT] Chapter 7.5 (Step4 : 화면이 3개인 UI 작성)

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,11 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var _index = 0;
+  var _pages = [
+    Page1(),
+    Page2(),
+    Page3(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -39,12 +44,7 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ]
       ),
-      body: Center(
-        child: Text(
-          '$_index 페이지',
-          style: TextStyle(fontSize: 40),
-        ),
-      ),
+      body: _pages[_index],
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) {
           setState(() {
@@ -66,6 +66,42 @@ class _MyHomePageState extends State<MyHomePage> {
             icon: Icon(Icons.account_circle),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '홈 페이지',
+        style: TextStyle(fontSize: 40),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '이용서비스',
+        style: TextStyle(fontSize: 40),
+      ),
+    );
+  }
+}
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '내 정보',
+        style: TextStyle(fontSize: 40),
       ),
     );
   }
